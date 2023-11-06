5032a2e02bfb0e647b36a44656aa72678b910d27
Lucas Finoti Rodrigues <lucasfinotirodrigues@hotmail.com>
2023-11-06 19:32:08 +0000

    wip: routes partially added

--- farmadelivery/src/components/App.jsx
+++ farmadelivery/src/components/App.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
-
-const App = () => {
+function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route/>
-      </Routes>
-    </BrowserRouter>
-  )
+      <BrowserRouter>
+          <Routes>
+            <Route/>
+              {/* <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/pagamento" element={<Pagamento />} />
+              <Route path="/carrinho" element={<Carrinho />} />
+              <Route path="/avaliacoes" element={<Avaliacoes/>} /> */}
+          </Routes>
+      </BrowserRouter>
+  );
 }
 
 export default App;
