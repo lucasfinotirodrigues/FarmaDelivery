08e07dda7aee4bf815da6cb65be2869e7863327a
Lucas Finoti Rodrigues <lucasfinotirodrigues@hotmail.com>
2023-11-06 19:52:01 +0000

    feat: definition of components

---
+++ farmadelivery/src/components/footer/footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './footer.css'
+
+
+function Rodape(){
+    return (
+        <div className='rodape'> 
+          <h3>FarmaDelivery</h3>
+          <h4>2023Copyrigth©</h4>
+        </div>
+    );
+}
+
+export default Rodape;
---
+++ farmadelivery/src/components/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Rodape from '../footer/footer';
+
+
+function Home(){
+    return (
+        <div>
+            Home do projeto     
+            <Rodape></Rodape>
+        </div>
+    );
+}
+
+export default Home;
