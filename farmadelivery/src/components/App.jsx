7448621d3215919f5c2e413abbfdbf5719f30a6d
Lucas Finoti Rodrigues <lucasfinotirodrigues@hotmail.com>
2023-11-11 16:03:24 +0000

    feat: configuration of routes added

--- farmadelivery/src/components/App.jsx
+++ farmadelivery/src/components/App.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './home/Home'
+import Login from './login/login';
+import Cadastro from './cadastro/cadastro';
+import Pagamento from './pagamento/pagamento';
+import Carrinho from './carrinho/carrinho';
+import Compras from './compras/compras';
+import Avaliacoes from './avaliacoes/avaliacoes';
 
 function App() {
   return (
-      <BrowserRouter>
-          <Routes>
-            <Route/>
-              {/* <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/cadastro" element={<Cadastro />} />
-              <Route path="/pagamento" element={<Pagamento />} />
-              <Route path="/carrinho" element={<Carrinho />} />
-              <Route path="/avaliacoes" element={<Avaliacoes/>} /> */}
-          </Routes>
-      </BrowserRouter>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/pagamento" element={<Pagamento />} />
+        <Route path="/carrinho" element={<Carrinho />} />
+        <Route path="/compras" element={<Compras />} />
+        <Route path="/avaliacoes" element={<Avaliacoes />} />
+      </Routes>
+    </Router>
   );
 }
 
