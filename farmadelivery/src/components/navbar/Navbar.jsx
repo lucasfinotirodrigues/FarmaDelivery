bd07c3018cc172aeb89a0186b4be98cdc6c0fd6a
Lucas Finoti Rodrigues <lucasfinotirodrigues@hotmail.com>
2023-11-12 14:12:35 +0000

    wip: navbar styles partially added

--- farmadelivery/src/components/navbar/Navbar.jsx
+++ farmadelivery/src/components/navbar/Navbar.jsx
@@ -1,14 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './Navbar.css';
 import './../../reset.css'; 
-import './../../styles.css'; 
+import './../../styles.css';
 
+function Navbar() {
+  const [showMenu, setShowMenu] = useState(false);
 
-function Navbar(){
-    return (
-        <div>
-           <p> Navbar</p>
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="info">
+          <img className='logo' src="../../images/Logo-White.png" alt="" srcset="" />
+          <h3>FarmaDelivery</h3>
         </div>
-    );
+        <ul className={`nav-list ${showMenu ? 'show' : ''}`}>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Avaliações</a></li>
+          <li><a href="#">Cadastrar</a></li>
+          <li><a href="#">Entrar</a></li>
+          <li><a href="#">Carrinho</a></li>
+        </ul>
+        <div className="burger-menu" onClick={toggleMenu}>&#9776;</div>
+      </nav>
+    </div>
+  );
 }
 
 export default Navbar;
