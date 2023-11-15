2ce195b28ead8ed7d8f6a2d511021ee0e576bd5d
Lucas Finoti Rodrigues <lucasfinotirodrigues@hotmail.com>
2023-11-15 20:58:53 +0000

    style: icons added in navbar and styles altered

--- farmadelivery/src/components/navbar/Navbar.jsx
+++ farmadelivery/src/components/navbar/Navbar.jsx
@@ -3,7 +3,13 @@ import './Navbar.css';
 import './../../reset.css'; 
 import './../../styles.css'; 
 
-import imagem from '../../images/Logo-White.svg';
+import logo from '../../images/Logo-White.svg';
+import home from '../../images/home.svg';
+import carrinho from '../../images/carrinho.svg';
+import entrar from '../../images/login.svg';
+import avaliacoes from '../../images/star.svg';
+import cadastrar from '../../images/user.svg';
+
 
 function Navbar() {
   const [showMenu, setShowMenu] = useState(false);
@@ -16,15 +22,29 @@ function Navbar() {
     <div>
       <nav className="navbar">
         <div className="info">
-          <img className='logo' src={imagem} alt="" srcset="" />
+          <img className='logo' src={logo} alt="" srcset="" />
           <h3>FarmaDelivery</h3>
         </div>
         <ul className={`nav-list ${showMenu ? 'show' : ''}`}>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Avaliações</a></li>
-          <li><a href="#">Cadastrar</a></li>
-          <li><a href="#">Entrar</a></li>
-          <li><a href="#">Carrinho</a></li>
+          <li>
+            <img src={home} alt="" />
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <img src={avaliacoes} alt="" /> 
+            <a href="#">Avaliações</a>
+          </li>
+          <li>
+            <img src={cadastrar} alt="" /> 
+            <a href="#">Cadastrar</a>
+          </li>
+          <li>
+            <img src={entrar} alt="" /> 
+            <a href="#">Entrar</a>
+          </li>
+          <li>
+            <img src={carrinho} alt="" /> 
+          </li>
         </ul>
         <div className="burger-menu" onClick={toggleMenu}>&#9776;</div>
       </nav>
