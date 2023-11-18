a223a4bb6077f0026b7e4d409b4334a20d7d1ce2
Lucas Finoti Rodrigues <lucasfinotirodrigues@hotmail.com>
2023-11-18 13:25:09 +0000

    style: layout of first section of Home added

--- farmadelivery/src/components/home/Home.jsx
+++ farmadelivery/src/components/home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Rodape from '../footer/footer';
 import Navbar from '../navbar/Navbar';
+import './Home.css';
 import './../../styles.css'; 
 import './../../reset.css'; 
 
@@ -9,9 +10,16 @@ import './../../reset.css';
 function Home(){
     return (
         <div>
+            <div className='estrutura'>
             <Navbar></Navbar>
-            {/* Home do projeto     
-            <Rodape></Rodape> */}
+            <div className='content'>
+                <div className="message">
+                    <h2>"Entregamos a seu medicamento onde você<br/>estiver,  pois a sua saúde não pode esperar!"</h2>
+                    <button className='button'>Entrar</button>
+                </div>
+            </div>
+            <Rodape></Rodape>
+            </div>
         </div>
     );
 }
